924b1b672f448ea6f0271bca2cfb9ce27953a2ed
ryan <vitineth@gmail.com>
2016-08-29 01:01:21 +0100

    Version update and removing false test file.

--- src/main/java/com/github/vitineth/map/the/stars/MapTheStars.java
+++ src/main/java/com/github/vitineth/map/the/stars/MapTheStars.java
@@ -42,7 +42,7 @@ import java.util.Map;
 public class MapTheStars {
 
     public static final String GAME_TITLE = "Map The Stars";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.1";
     private static final String LOG_NAME = MapTheStars.class.getSimpleName();
 
     private static MTSMainWindow mtsMainWindow;
